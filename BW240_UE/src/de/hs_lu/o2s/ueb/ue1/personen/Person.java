dc72733a3b300c4082e5aa4f42e77c387e34e82b
frank <frank@Frank-Tsidie>
2020-03-19 00:29:14 +0100

    erste teil der pruefung vorleistung erledigt

---
+++ BW240_UE/src/de/hs_lu/o2s/ueb/ue1/personen/Person.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package de.hs_lu.o2s.ueb.ue1.personen;
+
+/**
+ * @author frank
+ *
+ */
+public class Person {
+	/*
+	 * attributen festlegen
+	 * 
+	 */
+	private String vorname, nachname;
+	private Boolean beruftaetig;
+	private String beruf;
+	private String adresse;
+	private int geburtsjahr;
+
+	Person() {
+
+	}
+
+	Person(String vorname, String nachname) {
+		this.vorname = vorname;
+		this.nachname = nachname;
+	}
+
+	Person(String vorname, String nachname, Adresse adresse) {
+		this.vorname = vorname;
+		this.nachname = nachname;
+		this.adresse = adresse.toString();
+	}
+
+	public String getVorname() {
+		return this.vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getNachname() {
+		return this.nachname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	public Boolean getBeruftaetig() {
+		return this.beruftaetig;
+	}
+
+	public void setBeruftaetig(Boolean beruftaetig) {
+		this.beruftaetig = beruftaetig;
+	}
+
+	public String getBeruf() {
+		if (beruftaetig == false)
+			return "Person ist nicht berufstätig";
+		return this.beruf;
+	}
+
+	public void setBeruf(String beruf) {
+		this.beruf = beruf;
+	}
+
+	public String getAdresse() {
+		return this.adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public int getGeburtsjahr() {
+		if (geburtsjahr > 1900)
+			return this.geburtsjahr;
+		return -1;
+	}
+
+	public void setGeburtsjahr(int geburtsjahr) {
+		this.geburtsjahr = geburtsjahr;
+	}
+
+	public String toString() {
+
+		return this.vorname + " " + this.nachname + "\n" + "Beruf : " + this.beruf + "\n" + getBeruf()
+				+ "\nGeboren im Jahr" + getGeburtsjahr() + "Adresse :" + adresse.toString();
+	}
+}
--- uebung/Adresse.java
+++ uebung/Adresse.java
@@ -2,7 +2,7 @@ package de.hs_lu.o2s.tut1.uebung;
 
 public class Adresse {
 
-	private String strasse;//straße
+	private String strasse;//strasse
 	private String  hnr;//hauusnummer
 
 }
